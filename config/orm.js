d80e55e2fc61721c27e7d5a5523d1ed05931c6a9
kneves <55398273+kneves1@users.noreply.github.com>
2020-04-21 14:48:42 -0400

    required connection.js in orm.js, export ORM obj from orm.js

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+var connection = require("connection.js");
+
+
+// Methods used to retrieve and store data in the database
+// `selectAll()`
+// `insertOne()`
+// `updateOne()`
+
+
+
+module.exports = orm;
+
