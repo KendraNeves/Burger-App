8c4b87d61d6beed40e9620be5d6ddc24f0860e5a
kneves <55398273+kneves1@users.noreply.github.com>
2020-04-24 23:39:27 -0400

    Submit button working

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+$(".create-form").on("submit", function(event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+
+  var newBurger = {
+    burger_name: $("#ca").val().trim()
+  };
+
+  // Send the POST request.
+  $.ajax("/api/burgers", {
+    type: "POST",
+    data: newBurger
+  }).then(
+    function() {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
+});
+
+});
