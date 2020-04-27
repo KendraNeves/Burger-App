287b28d24b17cc1905c2923bd552525ba1d4eefa
kneves <55398273+kneves1@users.noreply.github.com>
2020-04-26 20:34:31 -0400

    Connected to JawsDB so I can deploy app to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "JHU2020",
-  database: "burgers_db"
-});
+//Connects to JawsDB so we can deploy to Heroku
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'JHU2020',
+    database: 'burgers_db'
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -16,6 +21,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
